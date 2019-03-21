d5611c7142601c7369a28d975db322bfda2b4753
chenn <chenn@gisuni.com>
2019-03-22 01:52:23 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+export function main({ locale, baseURL }) {
+    console.log(locale);
+    console.log(baseURL);
+    new Vue({
+        render: h => h(App),
+    }).$mount('#app');
+}
